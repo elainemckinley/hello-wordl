1a47536164b264ca02b0806c5d3ff60fcd828f33
Elaine McKinley <elainemckinley0@gmail.com>
2022-01-05 21:50:18 -0700

    Updates

--- src/util.ts
+++
@@ -1,7 +0,0 @@
-import dictionary from "./dictionary.json";
-
-export const dictionarySet: Set<string> = new Set(dictionary);
-
-export function pick<T>(array: Array<T>): T {
-  return array[Math.floor(array.length * Math.random())];
-}
